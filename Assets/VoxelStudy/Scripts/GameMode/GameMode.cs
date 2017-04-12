ed1d9e7327b545826ebefe123a8786edc21371ae
Thiago Borda <tvborda@gmail.com>
2017-04-12 06:35:36 -0400

    Add base for pattern loader and game mode

---
+++ Assets/VoxelStudy/Scripts/GameMode/GameMode.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VoxelStudy
+{
+    public class GameMode : MonoBehaviour
+    {
+        //The variable that keeps track of whether a start pattern needs to be spawned
+        private bool spawnStartPattern = true;
+        //used to keep track of whether the active character has been initialized
+        private bool spawnCharacter = true;
+
+
+        void Start()
+        {
+            PatternLoader.instance.LoadPatterns();
+            LoadGame();
+        }
+
+        private void LoadGame()
+        {
+            //StartCoroutine(SpawnPattern());
+        }
+
+        void Update()
+        {
+    		
+        }
+
+//        private IEnumerator SpawnPattern()
+//        {
+//            while (spawnStartPattern || activeCharacter.position.z - (newPatternStartZ * PatternSettings.tiledSize) < patternSpawnDistance)
+//            {
+//                patternIndex++;
+//                numberOfPatternsAdded++;
+//                Transform selectedPatternPrefab = null;
+//
+//                if (!spawnStartPattern)
+//                {
+//                    if (patternIndex % 5 != 0)
+//                    {
+//                        if (numberOfPatternsAdded <= numberOfEasyPatterns)
+//                        {
+//                            selectedPatternPrefab = PatternLoader.instance.GetEasyPattern(onScreenPatternEntrances[onScreenPatternEntrances.Count - 1]);
+//                        }
+//                        else if (numberOfPatternsAdded <= numberOfEasyPatterns + numberOfMediumPatterns)
+//                        {
+//                            selectedPatternPrefab = PatternLoader.instance.GetMediumPattern(onScreenPatternEntrances[onScreenPatternEntrances.Count - 1]);
+//                        }
+//                        else
+//                        {
+//                            selectedPatternPrefab = PatternLoader.instance.GetHardPattern(onScreenPatternEntrances[onScreenPatternEntrances.Count - 1]);
+//                        }
+//                    }
+//                    else
+//                    {
+//                        selectedPatternPrefab = PatternLoader.instance.GetConnectorPattern();
+//                    }
+//                }
+//                else
+//                {
+//                    selectedPatternPrefab = PatternLoader.instance.GetStartPattern();
+//                    spawnStartPattern = false;
+//                }
+//
+//                Transform selectedPattern = (Transform)Instantiate(selectedPatternPrefab);
+//                selectedPattern.name = selectedPatternPrefab.name; //to prevent the clone thing that just irritates me personally :)
+//                Pattern patternScript = selectedPattern.GetComponent<Pattern>();
+//
+//                bool evenGridZ = patternScript.gridZ % 2 == 0 ? true : false;
+//                float halfGridZ = patternScript.gridZ * 0.5f;
+//                if (evenGridZ)
+//                {
+//                    newPatternStartZ += 0.5f;
+//                }
+//                newPatternStartZ -= halfGridZ;
+//
+//                selectedPattern.parent = patternParentContainer;
+//                selectedPattern.localPosition = new Vector3(0, 0, newPatternStartZ * PatternSettings.tiledSize);
+//
+//                levelTiles = selectedPattern.GetComponentsInChildren<LevelTiles>();
+//                for (int i = 0; i < levelTiles.Length; i++)
+//                {
+//                    levelTiles[i].Initialize();
+//                }
+//
+//                newPatternStartZ -= halfGridZ;
+//                if (evenGridZ)
+//                {
+//                    newPatternStartZ -= 0.5f;
+//                }
+//
+//                onScreenPatterns.Add(selectedPattern);
+//                onScreenPatternEdges.Add(-(halfGridZ + 1) * PatternSettings.tiledSize);
+//                onScreenPatternEntrances.Add(patternScript.topEntrances);
+//
+//                if (spawnCharacter)
+//                {
+//                    activeCharacter = (Transform)Instantiate(characterPrefab, Vector3.zero, Quaternion.Euler(initialCharacterRotation));
+//                    activeCharacter.parent = patternParentContainer;
+//                    activeCharacter.GetComponent<TVNTCharacterController>().movementStyle = movementStyle;
+//                    activeCharacter.GetComponent<TVNTPlayerController>().tapToMove = false;
+//                    //Find the start tile and the place the character over it
+//                    //there should be a start tile in the start pattern
+//                    Vector3 startPosition = GameObject.Find("Start_Symbol").transform.position + new Vector3(0, PatternSettings.playerYOffset, 0);
+//                    activeCharacter.position = startPosition;
+//                    characterInitialPosition = startPosition.z;
+//                    cameraTarget.position = activeCharacter.position;
+//                    Camera.main.GetComponent<SmoothFollowCamera2D>().target = cameraTarget;
+//                    spawnCharacter = false;
+//                }
+//                yield return null;
+//            }
+//        }
+    }
+}
---
+++ Assets/VoxelStudy/Scripts/GameMode/PatternLoader.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using VoxelStudy;
+
+public class PatternLoader : MonoBehaviour {
+
+    public static PatternLoader instance = null;
+    public bool isPersistant = true;
+
+    [HideInInspector]
+    public string patternLocation = "";
+
+    [HideInInspector]
+    private Transform[] patterns;
+    [HideInInspector]
+    private List<Transform> introductoryPatterns = new List<Transform> ();
+    [HideInInspector]
+    private List<Transform> connectorPatterns = new List<Transform>();
+    [HideInInspector]
+    private List<Transform> easyPatterns = new List<Transform>();
+    [HideInInspector]
+    private List<Transform> mediumPatterns = new List<Transform>();
+    [HideInInspector]
+    private List<Transform> hardPatterns = new List<Transform>();
+
+    [HideInInspector]
+    public bool patternsLoaded = false;
+    private List<int> tempEntranceList = new List<int> ();
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy (gameObject);
+        }
+        if (isPersistant) {
+            DontDestroyOnLoad (gameObject);
+        }
+    }
+
+    public void LoadPatterns() {
+        if (patternsLoaded == false) {
+            Debug.Log ("Loading Patterns...");
+            patterns = Resources.LoadAll<Transform> (patternLocation);
+            for (int i = 0; i < patterns.Length; i++) {
+                Pattern indexedPatternScript = patterns [i].GetComponent<Pattern> ();
+                switch (indexedPatternScript.type) {
+                    case Pattern.Type.Introductory:
+                        introductoryPatterns.Add (patterns [i]);
+                        break;
+                    case Pattern.Type.Connector:
+                        connectorPatterns.Add (patterns [i]);
+                        break;
+                    case Pattern.Type.Easy:
+                        easyPatterns.Add (patterns [i]);
+                        break;
+                    case Pattern.Type.Medium:
+                        mediumPatterns.Add (patterns [i]);
+                        break;
+                    case Pattern.Type.Hard:
+                        hardPatterns.Add (patterns [i]);
+                        break;
+                }
+            }
+            patternsLoaded = true;
+        }
+    }
+
+    public Transform GetIntroductoryPattern() {
+        Transform pattern = null;
+        if (introductoryPatterns.Count > 0) {
+            pattern = introductoryPatterns [Random.Range (0, introductoryPatterns.Count)];
+        } else {
+            Debug.LogWarning ("There are no Introductory patterns");
+        }
+        return pattern;
+    }
+        
+    public Transform GetConnectorPattern() {
+        Transform pattern = null;
+        if (connectorPatterns.Count > 0) {
+            pattern = connectorPatterns [Random.Range (0, connectorPatterns.Count)];
+        }  else {
+            Debug.LogWarning ("There are no Connector patterns");
+        }
+        return pattern;
+    }
+
+    public Transform GetEasyPattern() {
+        Transform pattern = null;
+        if (easyPatterns.Count > 0) {
+            pattern = easyPatterns [Random.Range (0, easyPatterns.Count)];
+        } else {
+            Debug.LogWarning ("There are no Easy patterns");
+        }
+        return pattern;
+    }
+
+    public Transform GetMediumPattern(int entranceInt = -1) {
+        Transform pattern = null;
+        if (mediumPatterns.Count > 0) {
+            pattern = mediumPatterns [Random.Range (0, mediumPatterns.Count)];
+        } else {
+            Debug.LogWarning ("There are no Medium patterns");
+        }
+        return pattern;
+    }
+
+    public Transform GetHardPattern(int entranceInt = -1) {
+        Transform pattern = null;
+        if (hardPatterns.Count > 0) {
+            pattern = hardPatterns [Random.Range (0, hardPatterns.Count)];
+        } else {
+            Debug.LogWarning ("There are no Hard patterns");
+        }
+        return pattern;
+    }
+}
